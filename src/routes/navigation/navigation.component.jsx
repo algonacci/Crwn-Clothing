cddcd638688714fcf740504c06d6365564b0530b
Eric Julianto <algonacci1@gmail.com>
2023-04-04 18:03:23 +0700

    refactor and finishing signing out

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -9,12 +9,7 @@ import { UserContext } from "../../context/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 const Navigation = () => {
-  const { currentUser, setCurrentUser } = useContext(UserContext);
-
-  const signOutHandler = async () => {
-    await signOutUser();
-    setCurrentUser(null);
-  };
+  const { currentUser } = useContext(UserContext);
 
   return (
     <Fragment>
@@ -27,8 +22,9 @@ const Navigation = () => {
             SHOP
           </Link>
           {currentUser ? (
-            <span className="nav-link" onClick={signOutHandler}>
-              SIGN OUT
+            <span className="nav-link" onClick={signOutUser}>
+              {" "}
+              SIGN OUT{" "}
             </span>
           ) : (
             <Link className="nav-link" to="/auth">
