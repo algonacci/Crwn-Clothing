973e4b0bb2458ed48bf718ad4d9e76cff8375640
Eric Julianto <algonacci1@gmail.com>
2023-04-21 09:37:32 +0700

    create shop page

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./context/user.context";
+import { ProductsProvider } from "./context/product.context";
 import App from "./App";
 import "./index.scss";
 
@@ -9,7 +10,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
